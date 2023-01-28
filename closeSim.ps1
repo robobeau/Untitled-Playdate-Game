a4553a1ce8b8bfb619e4a2e009cc18adf14ba9c0
René Esteves <rene@robobeau.com>
2023-01-28 10:14:29 -0500

    Pointless formatting! Muh OCD!

--- closeSim.ps1
+++ closeSim.ps1
@@ -2,20 +2,21 @@ $sim = Get-Process "PlaydateSimulator" -ErrorAction SilentlyContinue
 
 if ($sim)
 {
-    $sim.CloseMainWindow()
-    $count = 0
-    while (!$sim.HasExited) 
-    {
-        Start-Sleep -Milliseconds 10
-        $count += 1
+  $sim.CloseMainWindow()
+  $count = 0
 
-        if ($count -ge 5)
-        {
-            $sim | Stop-Process -Force
-        }
+  while (!$sim.HasExited) 
+  {
+    Start-Sleep -Milliseconds 10
+    $count += 1
+
+    if ($count -ge 5)
+    {
+      $sim | Stop-Process -Force
     }
+  }
 }
 else
 {
-    exit 0
+  exit 0
 }
