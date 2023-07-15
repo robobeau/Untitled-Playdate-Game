2fc5df76c31206d6870734c80d7b8531f9004bd7
René Esteves <rene@robobeau.com>
2023-07-15 08:19:41 -0400

    Testing smaller images

--- source/characters/Kim/tsxs/KimStand.tsx
+++ source/characters/Kim/tsxs/KimStand.tsx
@@ -13,12 +13,12 @@
    <property name="frameDuration" type="int" value="3"/>
    <property name="velocityX" type="int" value="0"/>
   </properties>
-  <image width="80" height="96" source="../images/KimStand1.png"/>
+  <image width="72" height="84" source="../images/KimStand1.png"/>
   <objectgroup draworder="index" id="2">
-   <object id="2" name="Pushbox" type="Pushbox" x="20" y="12" width="40" height="84"/>
-   <object id="3" name="Hurtbox (Body)" type="Hurtbox" x="8" y="42" width="64" height="54"/>
-   <object id="1" name="Hurtbox (Head)" type="Hurtbox" x="20" y="12" width="40" height="38"/>
-   <object id="4" name="Center" type="Center" x="40" y="96">
+   <object id="2" name="Pushbox" type="Pushbox" x="16" y="0" width="40" height="84"/>
+   <object id="3" name="Hurtbox (Body)" type="Hurtbox" x="4" y="30" width="64" height="54"/>
+   <object id="1" name="Hurtbox (Head)" type="Hurtbox" x="16" y="0" width="40" height="38"/>
+   <object id="4" name="Center" type="Center" x="36" y="84">
     <point/>
    </object>
   </objectgroup>
@@ -28,12 +28,12 @@
    <property name="cancellable" type="int" propertytype="Cancellable" value="123"/>
    <property name="frameDuration" type="int" value="4"/>
   </properties>
-  <image width="80" height="96" source="../images/KimStand2.png"/>
+  <image width="72" height="86" source="../images/KimStand2.png"/>
   <objectgroup draworder="index" id="2">
-   <object id="2" name="Pushbox" type="Pushbox" x="20" y="12" width="40" height="84"/>
-   <object id="4" name="Hurtbox (Body)" type="Hurtbox" x="8" y="40" width="64" height="56"/>
-   <object id="3" name="Hurtbox (Head)" type="Hurtbox" x="20" y="10" width="40" height="38"/>
-   <object id="5" name="Center" type="Center" x="40" y="96">
+   <object id="2" name="Pushbox" type="Pushbox" x="16" y="2" width="40" height="84"/>
+   <object id="4" name="Hurtbox (Body)" type="Hurtbox" x="4" y="30" width="64" height="56"/>
+   <object id="3" name="Hurtbox (Head)" type="Hurtbox" x="16" y="0" width="40" height="38"/>
+   <object id="5" name="Center" type="Center" x="36" y="86">
     <point/>
    </object>
   </objectgroup>
@@ -43,12 +43,12 @@
    <property name="cancellable" type="int" propertytype="Cancellable" value="123"/>
    <property name="frameDuration" type="int" value="5"/>
   </properties>
-  <image width="80" height="96" source="../images/KimStand3.png"/>
+  <image width="72" height="86" source="../images/KimStand3.png"/>
   <objectgroup draworder="index" id="2">
-   <object id="2" name="Pushbox" type="Pushbox" x="20" y="12" width="40" height="84"/>
-   <object id="4" name="Hurtbox (Body)" type="Hurtbox" x="8" y="38" width="64" height="58"/>
-   <object id="3" name="Hurtbox (Head)" type="Hurtbox" x="20" y="10" width="40" height="38"/>
-   <object id="5" name="Center" type="Center" x="40" y="96">
+   <object id="2" name="Pushbox" type="Pushbox" x="16" y="2" width="40" height="84"/>
+   <object id="4" name="Hurtbox (Body)" type="Hurtbox" x="4" y="28" width="64" height="58"/>
+   <object id="3" name="Hurtbox (Head)" type="Hurtbox" x="16" y="0" width="40" height="38"/>
+   <object id="5" name="Center" type="Center" x="36" y="86">
     <point/>
    </object>
   </objectgroup>
@@ -58,12 +58,12 @@
    <property name="cancellable" type="int" propertytype="Cancellable" value="123"/>
    <property name="frameDuration" type="int" value="3"/>
   </properties>
-  <image width="80" height="96" source="../images/KimStand1.png"/>
+  <image width="72" height="84" source="../images/KimStand1.png"/>
   <objectgroup draworder="index" id="2">
-   <object id="2" name="Pushbox" type="Pushbox" x="20" y="12" width="40" height="84"/>
-   <object id="3" name="Hurtbox (Body)" type="Hurtbox" x="8" y="42" width="64" height="54"/>
-   <object id="4" name="Hurtbox (Head)" type="Hurtbox" x="20" y="12" width="40" height="38"/>
-   <object id="5" name="Center" type="Center" x="40" y="96">
+   <object id="2" name="Pushbox" type="Pushbox" x="16" y="0" width="40" height="84"/>
+   <object id="3" name="Hurtbox (Body)" type="Hurtbox" x="4" y="30" width="64" height="54"/>
+   <object id="4" name="Hurtbox (Head)" type="Hurtbox" x="16" y="0" width="40" height="38"/>
+   <object id="5" name="Center" type="Center" x="36" y="84">
     <point/>
    </object>
   </objectgroup>
