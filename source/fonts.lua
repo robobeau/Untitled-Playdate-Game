f740415681d5f6b07911b328e305380b9511dccc
René Esteves <rene@robobeau.com>
2023-09-19 19:51:35 -0400

    Creating fonts file

---
+++ source/fonts.lua
@@ -0,0 +1,9 @@
+local SuperMonacoGP <const> = playdate.graphics.font.new("fonts/Super Monaco GP")
+      SuperMonacoGP:setTracking(-1)
+
+fonts = {
+  -- ABCop = fon.new("fonts/A.B. Cop"),
+  -- BattleGaregga = fon.new("fonts/Battle Garegga - Type 2"),
+  -- STDIN = fon.new("fonts/ST-DIN"),
+  SuperMonacoGP = SuperMonacoGP,
+}
