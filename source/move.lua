172e45267039ee1d8779414893fb59a1ef8930b1
René Esteves <rene@robobeau.com>
2023-10-14 15:00:18 -0400

    Remove move

--- source/move.lua
+++
@@ -1,7 +0,0 @@
-class('Move')
-
-function Move:init(aerial, cancellable, inputCheck)
-  self.aerial = aerial or false
-  self.cancellable = cancellable or false
-  self.inputCheck = inputCheck or {}
-end
